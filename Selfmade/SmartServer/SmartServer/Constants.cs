da6d0422ff9ac3cee83fc45e99f36bae0635a551
Waterfront <w4terfront@gmail.com>
2020-05-28 15:40:52 +0200

    using datafolder for persistent files

--- Selfmade/SmartServer/SmartServer/Constants.cs
+++ Selfmade/SmartServer/SmartServer/Constants.cs
@@ -5,7 +5,7 @@
     public static int AUTODISCOVER_PORT = 7352;
     public static int VERSION = 1;
     public static string MQTT_CLIENT_NAME = "SmartServer";
-    public static string SQLITE_CONNECTION_STRING = "Data Source=SmartServer.db";
+    public static string SQLITE_CONNECTION_STRING = "Data Source=Data/SmartServer.db";
     public static string MQTT_TEMPERATURE_TOPIC = "/iot/temperature/";
   }
 }
